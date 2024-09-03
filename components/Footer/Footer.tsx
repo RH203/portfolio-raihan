81de8dec9d99996042b65bd7310768dc27a35d7e
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-09-03 19:57:58 +0700

    hehe

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer footer-center text-base-content p-4 bg-base-100">
+      <aside>
+        <p>Built by Raihan using Next.js</p>
+      </aside>
+    </footer>
+  );
+}
+
+export default Footer;
